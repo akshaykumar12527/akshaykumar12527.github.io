0c0eefb41ae36eb168eac251590db62dc883d522
akshaykumar <akshaykumar12527@gmail.com>
2025-01-16 14:15:24 +0530

    portfolio title and resumeLink updated

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -42,7 +42,7 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
+                    href={greeting.resumeLink}
                     download="Resume.pdf"
                     className="download-link-button"
                   >
